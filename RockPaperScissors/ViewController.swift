0db40d8d6fe7553afd23df627bec46bb18d68929
David Lam <davidlys@gmail.com>
2018-11-09 03:14:38 +0800

    Initial commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by David Lam on 9/11/18.
+//  Copyright © 2018 David Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
